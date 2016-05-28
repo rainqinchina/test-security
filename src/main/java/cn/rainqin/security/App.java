caa393d3ab45bf109db24d673ba4d611746af7fa
rainqinchina <rainqin_china@126.com>
2016-05-28 20:02:12 +0800

    init

---
+++ src/main/java/cn/rainqin/security/App.java
@@ -0,0 +1,13 @@
+package cn.rainqin.security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
